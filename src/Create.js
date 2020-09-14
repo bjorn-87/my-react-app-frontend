2e33e76f77fe47436f5d6d057d78017dab942d4c
Björn Olsson <bjorn.olsson87@gmail.com>
2020-09-14 15:34:01 +0100

    Adding route Create and Register

---
+++ src/Create.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Auth } from './Auth.js';
+import { Redirect, Link } from 'react-router-dom';
+
+import "./Input.css"
+import "./Buttons.css"
+import "./Create.css"
+
+class Create extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            text: "",
+            week: 0,
+            message: "",
+            redirect: ""
+        };
+    }
+
+    componentDidMount() {
+        const week = this.props.match.params.id;
+        fetch(`http://localhost:1337/reports/week/${week}`)
+        .then((response) => response.json())
+        .then((res) => {
+            this.setState({
+                text: res.data.text,
+                week: res.data.week
+            })
+            // setText(res.data.text);
+        })
+    }
+
+    handleSubmit(event) {
+        const payload =  {
+            text: this.state.text,
+            week: this.state.week
+        }
+        // console.log(payload);
+        // console.log(Auth.token);
+
+        fetch('http://localhost:1337/reports', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json",
+                "x-access-token": `${Auth.token}`
+            },
+            body: JSON.stringify(payload)
+        })
+        .then((response) => response.json())
+        .then((res) => {
+            console.log(res);
+            if (res.data) {
+                this.setState({
+                    message: res.data.msg,
+                    redirect: "/reports/"
+                });
+            } else if (res.errors.detail === "SQLITE_CONSTRAINT: UNIQUE constraint failed: reports.week" ) {
+                this.setState({message: "Posten finns redan, ändra veckonummer"});
+            } else {
+                this.setState({message: res.errors.detail});
+            }
+        });
+        event.preventDefault();
+        // alert("submit");
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        // console.log(this.state);
+
+        this.setState({
+            [name]: value
+        });
+        // console.log(this.state);
+    }
+    render() {
+        const redirect = this.state.redirect;
+        if (!Auth.token) {
+            return <div className="createPage"><p>Logga in för att Skapa rapport</p></div>
+        } else if (redirect) {
+            return <Redirect to={redirect}/>
+        }
+        return (
+            <div className="createPage">
+            <h2>Skapa rapport</h2>
+            <h3>{this.state.message}</h3>
+            <form className="" onSubmit={this.handleSubmit}>
+                <label className="input-label">
+                    Vecka
+                </label>
+                <input
+                    type="number"
+                    className="input"
+                    name="week"
+                    min="1"
+                    required
+                    value={this.state.week}
+                    onChange={this.handleChange} />
+                <label className="input-label">
+                    Text
+                </label>
+                <textarea
+                    className="input-textarea"
+                    name="text"
+                    max="1000"
+                    value={this.state.text}
+                    onChange={this.handleChange} />
+                <input className="button green-button" type="submit" value="submit" />
+            </form>
+            <Link to="/reports">Tillbaka</Link>
+            </div>
+        )
+    }
+}
+
+export default Create;
---
+++ src/Register.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+import "./Register.css";
+import "./Buttons.css";
+import "./Input.css";
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            email: "",
+            password: "",
+            message: ""
+        }
+    }
+
+    handleSubmit(event) {
+        const payload = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(payload);
+
+        fetch('http://localhost:1337/register', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                this.setState({message: res.data.msg})
+            } else {
+                this.setState({message: res.errors.detail})
+            }
+            });
+
+        event.preventDefault();
+    // alert(`submitted${this.state.email}`);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <main className="registerPage">
+            <h2>Registrera användare</h2>
+            <p>{this.state.message}</p>
+            <form className="input" onSubmit={this.handleSubmit}>
+            <label className="input-label">
+                E-Post
+            </label>
+            <input
+                className="input"
+                type="email"
+                name="email"
+                placeholder="E-post"
+                required
+                value={this.state.email}
+                onChange={this.handleChange} />
+            <label className="input-label">
+                Lösenord
+            </label>
+            <input
+                className="input"
+                type="password"
+                name="password"
+                placeholder="Lösenord"
+                required
+                value={this.state.password}
+                onChange={this.handleChange} />
+            <input className="box" type="checkbox" required/>
+            <p className="gdpr"> Jag godkänner att min data sparas </p>
+            <input className="button blue-button" type="submit" value="Registrera" />
+            </form>
+            <Link to="/login">Tillbaka</Link>
+            </main>
+        )
+    }
+}
+
+export default Register;
